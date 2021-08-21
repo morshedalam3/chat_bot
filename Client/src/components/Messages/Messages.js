e2385933b48a26f9abc821e108c3bfd414add662
morshedalam3 <morshedhasan1969@gmail.com>
2021-08-21 13:51:00 +0600

    [added]

--- Client/src/components/Messages/Messages.js
+++ Client/src/components/Messages/Messages.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import ScrollToBottom from 'react-scroll-to-bottom';
+import CustomScrollBars from '../CustomScrollBar/CustomScrollBar'
 
 import Message from './Message/Message';
 
@@ -8,9 +7,9 @@ import './Messages.css';
 
 const Messages = ({ messages, name }) => {
   return (
-  <ScrollToBottom className="messages">
+  <CustomScrollBars className="messages">
     {messages.map((message, i) => <div key={i}><Message message={message} name={name}/></div>)}
-  </ScrollToBottom>
+  </CustomScrollBars>
   )
 };
 
